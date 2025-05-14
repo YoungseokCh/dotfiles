1ef494e0ee97b594b1b48a7a65c32bc670df32c3
Youngseok Choi <me@youngseok.com>
2025-05-14 11:23:19 +0900

    Update .zshrc Update .config/nvim/init.lua Update .config/nvim/lua/plugins/mason.lua

--- private_dot_config/nvim/lua/plugins/empty_mason.lua
+++ private_dot_config/nvim/lua/plugins/empty_mason.lua
@@ -8,5 +8,7 @@ return {
     "hrsh7th/cmp-path",
     "L3MON4D3/LuaSnip",
     "saadparwaiz1/cmp_luasnip",
-    "rafamadriz/friendly-snippets"
+    "rafamadriz/friendly-snippets",
+    "mfussenegger/nvim-lint",
+    "rshkarin/mason-nvim-lint",
 }
