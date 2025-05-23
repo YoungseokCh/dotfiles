0e2ace8720f7d67dfa1492728fd31c9e84228273
Youngseok Choi <me@youngseok.com>
2025-05-23 09:11:04 +0900

    Update .config/nvim/lua/plugins/language.lua

--- private_dot_config/nvim/lua/plugins/language.lua
+++ private_dot_config/nvim/lua/plugins/language.lua
@@ -1,15 +1,24 @@
 return {
-    "mason-org/mason.nvim",
-    "mason-org/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-    "hrsh7th/nvim-cmp",                 -- Completion engine
-    "hrsh7th/cmp-nvim-lsp",             -- LSP completion source for nvim-cmp
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "L3MON4D3/LuaSnip",
-    "saadparwaiz1/cmp_luasnip",
-    "rafamadriz/friendly-snippets",
-    "mfussenegger/nvim-lint",
-    "rshkarin/mason-nvim-lint",
-    "alker0/chezmoi.vim"
+	"mason-org/mason.nvim",
+	"mason-org/mason-lspconfig.nvim",
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				jinja_lsp = {
+					filetypes = { "jinja", "html" },
+				},
+			},
+		},
+	},
+	"hrsh7th/nvim-cmp", -- Completion engine
+	"hrsh7th/cmp-nvim-lsp", -- LSP completion source for nvim-cmp
+	"hrsh7th/cmp-buffer",
+	"hrsh7th/cmp-path",
+	"L3MON4D3/LuaSnip",
+	"saadparwaiz1/cmp_luasnip",
+	"rafamadriz/friendly-snippets",
+	"mfussenegger/nvim-lint",
+	"rshkarin/mason-nvim-lint",
+	"alker0/chezmoi.vim",
 }
