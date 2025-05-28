6e223d3833a5c4160334faf411dbdade5a5c5c54
Youngseok Choi <me@youngseok.com>
2025-05-28 17:08:18 +0900

    Update .config/nvim/lua/config/ui.lua

--- private_dot_config/nvim/lua/config/ui.lua
+++ private_dot_config/nvim/lua/config/ui.lua
@@ -1,4 +1,9 @@
 for type, icon in pairs(require("config.icons").diagnostics) do
 	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+
+	vim.api.nvim_set_sign(hl, {
+		text = icon,
+		texthl = hl,
+		numhl = hl,
+	})
 end
