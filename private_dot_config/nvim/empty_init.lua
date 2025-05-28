9e2eea60d5af092762254787e226392eb1698fc8
Youngseok Choi <me@youngseok.com>
2025-05-28 16:40:52 +0900

    Update .config/nvim/init.lua

--- private_dot_config/nvim/empty_init.lua
+++ private_dot_config/nvim/empty_init.lua
@@ -12,6 +12,30 @@ vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
 
+-- begin nvim-tree
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+
+require("nvim-tree").setup({
+	sort = {
+		sorter = "case_sensitive",
+	},
+	view = {
+		width = 30,
+	},
+	renderer = {
+		group_empty = true,
+	},
+	filters = {
+		dotfiles = true,
+	},
+})
+-- end nvim-tree
+
 require("mason").setup()
 require("mason-lspconfig").setup()
 require("mason-nvim-lint").setup({
