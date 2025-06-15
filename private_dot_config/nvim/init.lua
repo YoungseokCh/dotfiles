8fcf51f2f2a46ae05494790940e3203900014b72
Youngseok Choi <me@youngseok.com>
2025-06-16 08:58:50 +0900

    Update neovim

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,8 +1,4 @@
 -- Load core configuration
 require("config.options")
 require("config.lazy")
-require("config.autocomplete")
-require("config.command")
 require("config.keymap")
-require("config.mason")
-require("config.treesitter")
--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -1,12 +1,83 @@
 -- LSP, completion, and formatting plugins
 return {
 	-- Mason LSP installer
-	"mason-org/mason.nvim",
-	"mason-org/mason-lspconfig.nvim",
+	{
+		"mason-org/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup()
+		end,
+	},
 	"neovim/nvim-lspconfig",
 
 	-- Completion engine and sources
-	"hrsh7th/nvim-cmp",
+	{
+		"hrsh7th/nvim-cmp",
+		config = function()
+			local cmp_status, cmp = pcall(require, "cmp")
+			if not cmp_status then
+				return
+			end
+
+			local luasnip_status, luasnip = pcall(require, "luasnip")
+			if not luasnip_status then
+				return
+			end
+
+			require("luasnip/loaders/from_vscode").lazy_load()
+
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-k>"] = cmp.mapping.select_prev_item(),
+					["<C-j>"] = cmp.mapping.select_next_item(),
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = false }),
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+						elseif luasnip.expand_or_locally_jumpable() then
+							luasnip.expand_or_jump()
+						elseif
+							vim.fn.col(".") - 1 == 0
+							or vim.fn.getline("."):sub(vim.fn.col(".") - 1, vim.fn.col(".") - 1):match("%s")
+						then
+							fallback()
+						else
+							cmp.complete()
+						end
+					end, { "i", "s" }),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+						elseif luasnip.locally_jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+					{ name = "buffer" },
+					{ name = "path" },
+				}),
+			})
+		end,
+	},
 	"hrsh7th/cmp-nvim-lsp",
 	"hrsh7th/cmp-buffer",
 	"hrsh7th/cmp-path",
@@ -18,7 +89,16 @@ return {
 
 	-- Linting and formatting
 	"mfussenegger/nvim-lint",
-	"rshkarin/mason-nvim-lint",
+	{
+		"rshkarin/mason-nvim-lint",
+		config = function()
+			require("mason-nvim-lint").setup({
+				ensure_installed = {},
+				automatic_installation = false,
+				quiet_mode = false,
+			})
+		end,
+	},
 	{
 		"stevearc/conform.nvim",
 		opts = {
@@ -36,32 +116,41 @@ return {
 				end
 				return { timeout_ms = 500, lsp_format = "fallback" }
 			end,
+			notify_no_formatters = true,
 		},
-	},
+		config = function(_, opts)
+			require("conform").setup(opts)
 
-	-- Language-specific plugins
-	"alker0/chezmoi.vim",
-	{
-		"ray-x/go.nvim",
-		dependencies = {
-			"ray-x/guihua.lua",
-			"neovim/nvim-lspconfig",
-			"nvim-treesitter/nvim-treesitter",
-		},
-		opts = {},
-		config = function(lp, opts)
-			require("go").setup(opts)
-			local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				pattern = "*.go",
-				callback = function()
-					require("go.format").goimports()
-				end,
-				group = format_sync_grp,
+			vim.api.nvim_create_user_command("Format", function(args)
+				local range = nil
+				if args.count ~= -1 then
+					local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+					range = {
+						start = { args.line1, 0 },
+						["end"] = { args.line2, end_line:len() },
+					}
+				end
+				require("conform").format({ async = true, lsp_format = "fallback", range = range })
+			end, { range = true })
+
+			vim.api.nvim_create_user_command("FormatDisable", function(args)
+				if args.bang then
+					-- FormatDisable! will disable formatting just for this buffer
+					vim.b.disable_autoformat = true
+				else
+					vim.g.disable_autoformat = true
+				end
+			end, {
+				desc = "Disable autoformat-on-save",
+				bang = true,
+			})
+
+			vim.api.nvim_create_user_command("FormatEnable", function()
+				vim.b.disable_autoformat = false
+				vim.g.disable_autoformat = false
+			end, {
+				desc = "Re-enable autoformat-on-save",
 			})
 		end,
-		event = { "CmdlineEnter" },
-		ft = { "go", "gomod" },
-		build = ':lua require("go.install").update_all_sync()',
 	},
 }
