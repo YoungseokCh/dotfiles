ddbf8083da568ff76f80f31b4572648f297c2cdd
Youngseok Choi <me@youngseok.com>
2025-07-08 09:40:57 +0900

    Bye copilot

--- private_dot_config/nvim/lua/plugins/tools.lua
+++ private_dot_config/nvim/lua/plugins/tools.lua
@@ -1,27 +1,5 @@
 -- Development tools and utilities
 return {
-	-- AI assistance
-	{
-		"zbirenbaum/copilot.lua",
-		cmd = "Copilot",
-		event = "InsertEnter",
-		config = function()
-			require("copilot").setup({
-				suggestion = {
-					auto_trigger = false,
-					keymap = {
-						accept = "<C-\\>",
-						accept_word = false,
-						accept_line = false,
-						next = "<M-]>",
-						prev = "<M-[>",
-						dismiss = "<C-]>",
-					},
-				},
-			})
-		end,
-	},
-
 	{
 		"yetone/avante.nvim",
 		-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
