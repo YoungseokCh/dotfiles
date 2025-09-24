f24459f02c290234391089695000b768cd8c31c0
Youngseok Choi <me@youngseok.com>
2025-09-24 11:29:11 +0900

    Update dashboard

--- private_dot_config/nvim/lua/plugins/editor.lua
+++ private_dot_config/nvim/lua/plugins/editor.lua
@@ -28,7 +28,9 @@ return {
 			bigfile = { enabled = true },
 			dashboard = {
 				sections = {
-					{ section = "keys", gap = 1, padding = 1 },
+					{ icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+					{ icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+					{ icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
 					{ section = "startup" },
 				},
 			},
