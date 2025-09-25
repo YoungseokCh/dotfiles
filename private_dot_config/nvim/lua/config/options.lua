b8c3550102ff6f3388f96fe100618a85b5e7b9be
Youngseok Choi <me@youngseok.com>
2025-09-25 11:54:07 +0900

    Disable relative line number (NeoVim)

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -1,7 +1,7 @@
 -- Editor options
 vim.o.autoindent = true
-vim.o.number = true
-vim.o.relativenumber = true
+-- vim.o.number = true
+vim.o.relativenumber = false
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = false
